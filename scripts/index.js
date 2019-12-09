f5010c5dc125a987b82ac4b0dce1d78c2da1c170
Taylor Piccarreto <tpic12@gmail.com>
2019-12-09 14:42:55 -0500

    validateName func and create func

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,17 @@ import shoppingList from './shopping-list.js';
 import store from './store.js';
 import item from './item.js';
 
+const itemNames = [ '', 'apples', 'pears' ];
+itemNames.forEach(name => {
+  try {
+    item.validateName(name);
+    // create a new item if name is valid
+    store.items.push(item.create(name));
+  } catch(error) {
+    console.log(`Cannot add item: ${error}`);
+  }
+});
+
 const main = function () {
   shoppingList.bindEventListeners();
   shoppingList.render();
--- scripts/item.js
+++ scripts/item.js
@@ -1,1 +1,16 @@
-export default {};
+
+function validateName(name){
+  if (!name) {
+      throw 'Name must not be blank';
+  }
+}
+
+function create(name) {
+  let item = {id: cuid(), name:name, checked:false}
+  return item;
+}
+
+export default {
+    validateName,
+    create
+};
